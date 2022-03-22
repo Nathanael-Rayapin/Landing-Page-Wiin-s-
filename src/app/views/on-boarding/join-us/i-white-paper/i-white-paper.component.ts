35f8b954577764875aab358d6cdf4e9497b1e5da
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-22 23:25:23 +0100

    new component (white paper)

---
+++ src/app/views/on-boarding/join-us/i-white-paper/i-white-paper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-i-white-paper',
+  templateUrl: './i-white-paper.component.html',
+  styleUrls: ['./i-white-paper.component.scss']
+})
+export class IWhitePaperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
