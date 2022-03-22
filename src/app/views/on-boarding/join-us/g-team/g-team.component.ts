ea90f370a0cdc13d8c9f93aeaf7f7c9251ee2e62
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-22 23:25:53 +0100

    team component

---
+++ src/app/views/on-boarding/join-us/g-team/g-team.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-g-team',
+  templateUrl: './g-team.component.html',
+  styleUrls: ['./g-team.component.scss']
+})
+export class GTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
