4048383b8c9cdd0f467cd53a200605cca5bab1fd
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-22 23:25:38 +0100

    token component

---
+++ src/app/views/on-boarding/join-us/h-zion/h-zion.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-h-zion',
+  templateUrl: './h-zion.component.html',
+  styleUrls: ['./h-zion.component.scss']
+})
+export class HZionComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
