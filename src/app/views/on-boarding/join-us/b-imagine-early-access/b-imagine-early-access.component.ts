c0e6477926821966519f896d42fc2c18dc151254
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-22 23:26:48 +0100

    early access component

---
+++ src/app/views/on-boarding/join-us/b-imagine-early-access/b-imagine-early-access.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-b-imagine-early-access',
+  templateUrl: './b-imagine-early-access.component.html',
+  styleUrls: ['./b-imagine-early-access.component.scss']
+})
+export class BImagineEarlyAccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
