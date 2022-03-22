cf9517258257c661d4082945dc497dbdf6d55d00
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-22 23:26:16 +0100

    profile component

---
+++ src/app/views/on-boarding/join-us/e-profile/e-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-e-profile',
+  templateUrl: './e-profile.component.html',
+  styleUrls: ['./e-profile.component.scss']
+})
+export class EProfileComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
