52b6349468728a71c5d8f1063c62c1f1078cd72b
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-25 23:38:03 +0100

    new's feed & disover component

--- src/app/views/on-boarding/join-us/c-newsfeed-discover/c-newsfeed-discover.component.ts
+++ src/app/views/on-boarding/join-us/c-newsfeed-discover/c-newsfeed-discover.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { NewsFeedAnimation } from 'src/app/core/animation/on-boarding-animation/on-boarding.animation';
+import { FragmentService } from 'src/app/core/service/fragment/fragment.service';
 
 @Component({
   selector: 'app-c-newsfeed-discover',
@@ -8,10 +11,39 @@ import { NewsFeedAnimation } from 'src/app/core/animation/on-boarding-animation/
   animations: [NewsFeedAnimation]
 })
 export class CNewsfeedDiscoverComponent implements OnInit {
+  slide_change: boolean = false;
+  ecosystemSubs: Subscription;
+  ecosystemId: string = '';
+
+  tokenId: string = '';
   default: string = 'previous';
-  constructor() { }
+
+  constructor(
+    private fragmentService: FragmentService,
+    private viewportScroller: ViewportScroller
+  ) { }
 
   ngOnInit(): void {
+    this.ecosystemSubs = this.fragmentService.ecosystemFragment.subscribe(
+      (value: string) => {
+        this.ecosystemId = value;
+        if (this.ecosystemId === 'ecosystem') {
+          setTimeout(() => {
+            this.viewportScroller.scrollToAnchor(this.ecosystemId);
+          }, 100);
+        } else {
+          return;
+        }
+      }
+    );
+  }
+
+  @HostListener("document:scroll")
+  scrollfunction() {
+    console.log(document.body.scrollTop);
+    if (document.body.scrollTop > 1700 || document.documentElement.scrollTop > 1700) {
+      this.slide_change = true;
+    }
   }
 
   onPrevious() {
