e14ceebf7525b460dfe8b6c635208bea70091e71
Nathanael421 <nathanael.rayapin@gmail.com>
2022-04-02 23:26:39 +0200

    deleted files on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnBoardingMainComponent } from './views/on-boarding/on-boarding-main/on-boarding-main.component';
-import { JoinUsComponent } from './views/on-boarding/join-us/join-us.component';
 import { ContactUsComponent } from './views/on-boarding/contact-us/contact-us.component';
 import { HomeComponent } from './views/home/home.component';
 import { MainSearchBarComponent } from './core/component/main-search-bar/main-search-bar.component';
@@ -16,18 +15,6 @@ import { EffectsModule } from '@ngrx/effects';
 import { RootStoreModule } from './core/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
-import { ALetsBuildComponent } from './views/on-boarding/join-us/a-lets-build/a-lets-build.component';
-import { BImagineEarlyAccessComponent } from './views/on-boarding/join-us/b-imagine-early-access/b-imagine-early-access.component';
-import { CNewsfeedDiscoverComponent } from './views/on-boarding/join-us/c-newsfeed-discover/c-newsfeed-discover.component';
-import { DMessengerComponent } from './views/on-boarding/join-us/d-messenger/d-messenger.component';
-import { EProfileComponent } from './views/on-boarding/join-us/e-profile/e-profile.component';
-import { FMusicVideoComponent } from './views/on-boarding/join-us/f-music-video/f-music-video.component';
-import { GTeamComponent } from './views/on-boarding/join-us/g-team/g-team.component';
-import { HZionComponent } from './views/on-boarding/join-us/h-zion/h-zion.component';
-import { IWhitePaperComponent } from './views/on-boarding/join-us/i-white-paper/i-white-paper.component';
-import { JContactComponent } from './views/on-boarding/join-us/j-contact/j-contact.component';
-import { TextContactComponent } from './views/on-boarding/join-us/j-contact/text-contact/text-contact.component';
-import { CardsContactComponent } from './views/on-boarding/join-us/j-contact/cards-contact/cards-contact.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -36,23 +23,10 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
   declarations: [
     AppComponent,
     OnBoardingMainComponent,
-    JoinUsComponent,
     ContactUsComponent,
     HomeComponent,
     MainSearchBarComponent,
     MainNavBarComponent,
-    ALetsBuildComponent,
-    BImagineEarlyAccessComponent,
-    CNewsfeedDiscoverComponent,
-    DMessengerComponent,
-    EProfileComponent,
-    FMusicVideoComponent,
-    GTeamComponent,
-    HZionComponent,
-    IWhitePaperComponent,
-    JContactComponent,
-    TextContactComponent,
-    CardsContactComponent
   ],
   imports: [
     RootStoreModule,
